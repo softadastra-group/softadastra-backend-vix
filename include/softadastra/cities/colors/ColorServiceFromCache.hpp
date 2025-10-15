9a0c2308f3ff883e6a1017e33b7607175f7a6f50
Gaspard Kirira <gaspardkirira9@gmail.com>
2025-10-15 20:34:45 +0300

    feat(colors): add new 'colors' module with initial headers and sources

---
+++ include/softadastra/cities/colors/ColorServiceFromCache.hpp
@@ -0,0 +1,55 @@
+#ifndef COLOR_SERVICE_FROM_CACHE_HPP
+#define COLOR_SERVICE_FROM_CACHE_HPP
+
+#include <softadastra/commerce/colors/Color.hpp>
+#include <softadastra/commerce/colors/ColorCache.hpp>
+
+#include <adastra/utils/string/StringUtils.hpp>
+
+#include <optional>
+#include <string>
+#include <algorithm>
+
+namespace softadastra::commerce::colors
+{
+    class ColorServiceFromCache
+    {
+    public:
+        ColorServiceFromCache(ColorCache &cache) : cache(cache) {}
+
+        std::vector<Color> getAllColors() const
+        {
+            return cache.getAll();
+        }
+
+        std::optional<Color> findById(std::uint32_t id) const
+        {
+            const auto &all = cache.getAll();
+            auto it = std::find_if(all.begin(), all.end(), [id](const Color &c)
+                                   { return c.getId() == id; });
+            if (it != all.end())
+                return *it;
+            return std::nullopt;
+        }
+
+        std::optional<Color> findByName(const std::string &name) const
+        {
+            const auto &all = cache.getAll();
+            std::string searched = adastra::utils::string::trimAndToLower(name);
+
+            auto it = std::find_if(all.begin(), all.end(), [&](const Color &c)
+                                   {
+                                       std::string cname = adastra::utils::string::trimAndToLower(c.getName());
+                                       return cname == searched; });
+
+            if (it != all.end())
+                return *it;
+            return std::nullopt;
+        }
+
+    private:
+        ColorCache &cache;
+    };
+}
+
+#endif
